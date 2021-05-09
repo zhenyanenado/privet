d8530d2d4b1019ab2a0a942c1e769e2152755dc7
zhenyanenado <70278943+zhenyanenado@users.noreply.github.com>
2021-05-09 17:44:37 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,81 +1,67 @@
 document.addEventListener('DOMContentLoaded', function(){
-	
-		let slides = [];
-		let grid1 = document.getElementById('grid1');
-		let krug = document.getElementById('krug');
-		let swipe = document.getElementById('swipe');
-		let hello = document.getElementById('hello');
 
-			krug.addEventListener('touchmove', function(){
+	const grid = document.getElementById('grid');
 
-				let pos = event.touches[0].pageX;
-				let position = pos-=150;
-				let opac = 1;
+	let colors = [
 
-					if(position < 650 && position > 0){				
-						this.style.left = position + 'px';									
-					}
+		'green', 'red', 'black', 'yellow'
 
-					if(position > 300){
-						swipe.style.opacity = 0.2;
-					}
+	];
 
-					if(position > 650){
-							grid1.style.background = 'white';
-							krug.style.opacity = 0;
-							swipe.style.opacity = 0;
-					
-						setTimeout(function(){
-							hello.style.opacity = 1;					
-						}, 500);
+	let grids = [];
 
-						setTimeout(function(){
-							hello.style.opacity = 0;
-								for(var i=2; i<=5; i++){
-									let all = document.getElementById('grid'+i).style;
-										all.opacity = 1;
-										all.width = '750px';
-										all.left = 100 + 'px';
-										all.transition = 'all ease 1s';						
-									}
-						}, 1000)						
-				}			
-		})	
+	function create(){
+		for(let i = 1; i<=4; i++){
+
+			let newGrids = document.createElement('div');
+				newGrids.id = 'grid' + i;
+				newGrids.style.width = '400px';
+				newGrids.style.height = '700px';
+				newGrids.style.position = 'fixed';
+				newGrids.style.transition = 'all ease 1s';
+				grid.append(newGrids)
+				grids.push(newGrids)
+
+		
 
 
-			let inpsValue = [];
-			let value = [];
-			let gridAll = [];
 
-			for(let w = 2; w<=5; w++){
-				inpsValue.push(document.getElementById('inp'+w));
-				gridAll.push(document.getElementById('grid'+w));
-			
-				document.getElementById('inp'+w).addEventListener('touchmove', function(){
+		}
+	}
 
-					value.push(this.value);
+	create();
 
-					if(this == inpsValue[0]){
-						inpsValue[0].style.opacity = 0;
-						gridAll[0].innerHTML = this.value;
-					}
-					
-					if(this == inpsValue[1]){
-						inpsValue[1].style.opacity = 0;
-						gridAll[1].innerHTML = this.value;
-					}
-
-					if(this == inpsValue[2]){
-						inpsValue[2].style.opacity = 0;
-						gridAll[2].innerHTML = this.value;
-					}
-
-					if(this == inpsValue[3]){
-						inpsValue[3].style.opacity = 0;
-						gridAll[3].innerHTML = this.value;
-					}
+	document.addEventListener('touchmove', event =>{
 
 
-				})
+		let number = 370;
+		let number2 = 100;
+		let position = event.touches[0].clientX;
+		let num1 = 0;
+
+			console.log(position)
+
+			if(position >= number){
+				num1+=3;
+				for(let t = 0; t<3; t++){
+					grids[t].style.background = colors[num1%4];
 			}
+
+			}
+
+			if(position < number2){
+				num1++;
+				for(let t = 0; t<3; t++){
+					grids[t].style.background = colors[num1%4];
+				}
+			}
+
+		
+
+
+	})
+
+
+
+
 })
